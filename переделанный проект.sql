8d519d049aabcad7da8a6707327750e8a58be22e
Bakytzhanova Natalya <bakytzhanova.n.d@gmail.com>
2026-04-13 14:47:59 +0600

    Учебный проект "Секреты Темнолесья"

--- переделанный проект.sql
+++ переделанный проект.sql
@@ -2,22 +2,14 @@
  * Цель проекта: изучить влияние характеристик игроков и их игровых персонажей 
  * на покупку внутриигровой валюты «райские лепестки», а также оценить 
  * активность игроков при совершении внутриигровых покупок
- * 
- * Автор: Бакытжанова Наталья
- * Дата: 14.09.2025
-*/
 
--- Часть 1. Исследовательский анализ данных
--- Задача 1. Исследование доли платящих игроков
-
--- 1.1. Доля платящих пользователей по всем данным:
--- Напишите ваш запрос здесь
+-- Для начала проводим ИАД и анализируем доли платящих игроков
+-- 1.Доля платящих пользователей по всем данным:
 SELECT COUNT(id) AS id,
        SUM(payer) AS sum,
        AVG(payer)AS dolya
        FROM fantasy.users; 
--- 1.2. Доля платящих пользователей в разрезе расы персонажа:
--- Напишите ваш запрос здесь
+-- 2. Доля платящих пользователей в разрезе расы персонажа:
 --Находим количество платящих игроков каждой расы
 WITH sum_race_pay AS (
 SELECT COUNT(payer)AS users, --общее количество игроков
@@ -25,7 +17,7 @@ SELECT COUNT(payer)AS users, --общее количество игроков
        AVG(payer),--доля платящих игроков
        race_id
 FROM fantasy.users
-GROUP BY race_id)--сократила запрос,по твоим рекомендациям,спасибо!
+GROUP BY race_id)
 SELECT race,
        srp.sum,
        srp.users,
@@ -33,9 +25,8 @@ SELECT race,
        FROM sum_race_pay AS srp
        JOIN fantasy.race AS r ON srp.race_id=r.race_id
        ORDER BY sum DESC;
--- Задача 2. Исследование внутриигровых покупок
--- 2.1. Статистические показатели по полю amount:
--- Напишите ваш запрос здесь
+-- Исследуем внутриигровые покупки:
+-- 1. Статистические показатели по полю amount:
 SELECT COUNT(amount) AS cnt_amount,
        SUM(amount) AS sum_amount,
        MIN(amount) AS min_amount,
@@ -43,15 +34,13 @@ SELECT COUNT(amount) AS cnt_amount,
        ROUND(AVG(amount)) AS avg_amount,
        ROUND(PERCENTILE_DISC(0.50)WITHIN GROUP (ORDER BY amount)) AS mediana,
        ROUND(STDDEV (amount)) AS stddev
-FROM fantasy.events; --округлила результаты
--- 2.2: Аномальные нулевые покупки:
--- Напишите ваш запрос здесь
+FROM fantasy.events; 
+-- 2: Аномальные нулевые покупки:
 SELECT COUNT(amount), 
 (COUNT(amount)::REAL / (SELECT COUNT(amount) FROM fantasy.events)) 
 FROM fantasy.events 
-WHERE amount = 0; --спасибо за рекомендации,код стал заметно короче и понятнее!
--- 2.3: Популярные эпические предметы:
--- Напишите ваш запрос здесь
+WHERE amount = 0; 
+-- 3: Популярные эпические предметы:
 WITH zero_sale AS (-- Фильтрация покупок с ненулевой стоимостью и с пропусками в названии предмета
     SELECT 
         e.id AS player_id,
@@ -65,16 +54,16 @@ WITH zero_sale AS (-- Фильтрация покупок с ненулевой 
 total_sale AS (
     -- Общее количество всех покупок и уникальных игроков
     SELECT 
-        COUNT(z.amount) AS total_sales_amount,--исправила SUM на COUNT
-        (SELECT COUNT(DISTINCT id) FROM fantasy.events) AS total_players --изменила эту строчку
+        COUNT(z.amount) AS total_sales_amount,
+        (SELECT COUNT(DISTINCT id) FROM fantasy.events) AS total_players 
     FROM zero_sale AS z
 ),
 count_sale AS (
     -- Общее количество продаж и уникальных игроков для каждого предмета
     SELECT 
         z.item_code,
-        COUNT(z.amount) AS item_sales_amount,--исправила SUM на COUNT 
-        COUNT(DISTINCT z.player_id) AS count_player --добавила DISTINCT
+        COUNT(z.amount) AS item_sales_amount, 
+        COUNT(DISTINCT z.player_id) AS count_player 
     FROM zero_sale AS z
     GROUP BY z.item_code
 ),
@@ -93,9 +82,7 @@ FROM count_sale AS cs
 JOIN items_1 AS i1 ON i1.item_code = cs.item_code
 JOIN total_sale AS ts ON 1=1  
 ORDER BY total_sales DESC;
--- Часть 2. Решение ad hoc-задачbи
--- Задача: Зависимость активности игроков от расы персонажа:
--- Напишите ваш запрос здесь
+-- Изучаем зависимость активности игроков от расы персонажа:
 WITH alies_1 AS(
 SELECT race_id,
        COUNT(amount) AS cnt, --общее количество покупок
@@ -110,7 +97,7 @@ FROM fantasy.users AS u
 JOIN fantasy.events AS e ON u.id=e.id
 WHERE amount != 0
 GROUP BY race_id),
-alies_3 AS( --добавила подзапрос для нахождения платящих игроков
+alies_3 AS( 
 SELECT r.race_id,
        COUNT(DISTINCT u.id) AS plata
 FROM fantasy.users AS u
@@ -126,7 +113,7 @@ GROUP BY race_id),
 doly AS(
 SELECT a1.race_id,
        sale/cnt::REAL AS avg_sale,--средняя сумма одной покупки
-       buyer_players/total_players ::REAL AS dolya_1,--не округлила потому что все по нулям
+       buyer_players/total_players ::REAL AS dolya_1,
        plata/buyer_players::REAL AS dolya_2
        FROM alies_1 AS a1
        JOIN alies_2 AS a2 ON a1.race_id=a2.race_id
